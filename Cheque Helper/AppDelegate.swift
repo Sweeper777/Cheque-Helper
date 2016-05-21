d6cff66c71999c4a638e3057dc6d53fac21c16a6
Sweeper777 <sumulang@gmail.com>
2016-05-21 18:07:56 +0800

    added firebase

--- Cheque Helper/AppDelegate.swift
+++ Cheque Helper/AppDelegate.swift
@@ -1,12 +1,5 @@
-//
-//  AppDelegate.swift
-//  Cheque Helper
-//
-//  Created by Mulang Su on 12/29/15.
-//  Copyright © 2015 Mulang Su. All rights reserved.
-//
-
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        FIRApp.configure()
         return true
     }
 
--- Podfile
+++ Podfile
@@ -1,10 +1,11 @@
 # Uncomment this line to define a global platform for your project
-# platform :ios, '8.0'
+platform :ios, '8.0'
 # Uncomment this line if you're using Swift
 use_frameworks!
 
 target 'Cheque Helper' do
-    pod ‘SwiftyButton’
+    pod 'SwiftyButton'
+    pod 'Firebase'
 end
 
 target 'Cheque HelperTests' do
