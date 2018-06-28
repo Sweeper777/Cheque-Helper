d907d9e0a8adc9cd2f67c0197580f9ad8fee798f
Sweeper777 <sumulang@gmail.com>
2018-06-28 08:38:37 +0100

    updated Chinese Cheque Converter

--- Cheque Helper/ChineseChequeConverter.swift
+++ Cheque Helper/ChineseChequeConverter.swift
@@ -51,52 +51,52 @@ class ChineseChequeConverter: Converter {
     
     fileprivate func convertInteger (_ x: String) -> String! {
         var number = x
-        if number.characters.count < 5 {
-            if number.characters.count == 1 {
-                return convertNumber(StringUtils.getDigitFrom(number, at: 0))
+        if number.count < 5 {
+            if number.count == 1 {
+                return convertNumber(number.getDigit(at: 0))
             }
             
-            let finalString = convertNumber(StringUtils.getDigitFrom(number, at: 0)) + getThatWord(number.characters.count - 1)
-            number = StringUtils.substring(number, start: 1)
+            let finalString = convertNumber(number.getDigit(at: 0)) + getThatWord(number.count - 1)
+            number = number.substring(start: 1)
             
             if Int(number) == 0 {
                 return finalString
             }
             
             var addZero = false
-            for i in 0  ..< number.characters.count  {
-                if number.characters[number.characters.index(number.characters.startIndex, offsetBy: i)] == "0" {
+            for i in 0  ..< number.count  {
+                if number[number.index(number.startIndex, offsetBy: i)] == "0" {
                     addZero = true
                     continue
                 }
-                return finalString + (addZero ? "零" : "") + convertInteger (StringUtils.substring(number, start: i))
+                return finalString + (addZero ? "零" : "") + convertInteger (number.substring(start: i))
             }
             return nil
         } else {
-            var charsToRead = number.characters.count % 4
+            var charsToRead = number.count % 4
             if charsToRead == 0 {
                 charsToRead = 4
             }
             
-            let firstPart = StringUtils.substring(number, start: 0, end: charsToRead)
-            var secondPart = StringUtils.substring(number, start: charsToRead)
+            let firstPart = number.substring(start: 0, end: charsToRead)
+            var secondPart = number.substring(start: charsToRead)
             
             var addZero = false
-            for i in 0  ..< secondPart.characters.count  {
-                if i == secondPart.characters.count - 1 && secondPart.characters[secondPart.characters.index(secondPart.characters.startIndex, offsetBy: i)] == "0" {
+            for i in 0  ..< secondPart.count  {
+                if i == secondPart.count - 1 && secondPart[secondPart.index(secondPart.startIndex, offsetBy: i)] == "0" {
                     addZero = false
                     continue
                 }
                 
-                if secondPart.characters[secondPart.characters.index(secondPart.characters.startIndex, offsetBy: i)] == "0" {
+                if secondPart[secondPart.index(secondPart.startIndex, offsetBy: i)] == "0" {
                     addZero = true
                     continue
                 }
-                secondPart = StringUtils.substring(secondPart, start: i)
+                secondPart = secondPart.substring(start: i)
                 break
             }
             
-            var thatWordIndex = 3 + number.characters.count / 4
+            var thatWordIndex = 3 + number.count / 4
             if charsToRead == 4 {
                 thatWordIndex -= 1
             }
@@ -121,7 +121,7 @@ class ChineseChequeConverter: Converter {
         formatter.maximumFractionDigits = 2
         number = formatter.string(from: parsedNumber)!
         
-        let twoParts = StringUtils.split(number, at: ".")
+        let twoParts = number.components(separatedBy: ".")
         let integerPart = twoParts[0]
         var decimalPart = twoParts.count == 1 ? "" : twoParts[1]
         //print("decimalPart: \(decimalPart)")
@@ -130,20 +130,20 @@ class ChineseChequeConverter: Converter {
         var decimalString: String = ""
         
         if !(decimalPart == "" || Int(decimalPart) == 0) {
-            if decimalPart.characters.count < 2 {
+            if decimalPart.count < 2 {
                 decimalPart += "0"
             }
             
             let jiao = String(decimalPart[decimalPart.startIndex])
-            let fen = String(decimalPart[decimalPart.characters.index(decimalPart.startIndex, offsetBy: 1)])
+            let fen = decimalPart.count == 1 ? "0" : String(decimalPart[decimalPart.index(decimalPart.startIndex, offsetBy: 1)])
             //print("jiao: \(jiao), fen: \(fen)")
             
             if jiao != "0" {
-                decimalString += convertNumber(StringUtils.getDigitFrom(jiao, at: 0)) + "角"
+                decimalString += convertNumber(jiao.getDigit(at: 0)) + "角"
             }
             
             if fen != "0" {
-                decimalString += convertNumber(StringUtils.getDigitFrom(fen, at: 0)) + "分"
+                decimalString += convertNumber(fen.getDigit(at: 1)) + "分"
             }
         }
         
