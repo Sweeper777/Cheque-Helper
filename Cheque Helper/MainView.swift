43f6aae49a1cd7d77a9b51d9ff24520ee1c526ef
Mulang Su <sumulang@gmail.com>
2019-11-06 07:22:06 +0000

    Create MainView.swift

---
+++ Cheque Helper/MainView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
+
