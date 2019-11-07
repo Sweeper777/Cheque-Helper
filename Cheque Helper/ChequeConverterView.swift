ed503d1d5cc59e4ea29684cebb85175692262063
Mulang Su <sumulang@gmail.com>
2019-11-07 22:03:50 +0000

    added ChequeConverterStateStore

--- Cheque Helper/ChequeConverterView.swift
+++ Cheque Helper/ChequeConverterView.swift
@@ -13,3 +13,10 @@ struct ChequeConverterView_Previews: PreviewProvider {
     }
 }
 
+class ChequeConverterStateStore: ObservableObject {
+    @Published var amountString = ""
+    @Published var selectedLanguage = 0
+    @Published var convertedString = ""
+    var disposeBag = [AnyCancellable]()
+    
+}
