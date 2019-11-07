ba7c49bd7e253c082052dfa5fffee66a4b87dd46
Mulang Su <sumulang@gmail.com>
2019-11-07 22:04:24 +0000

    added some views to ChequeConverterView

--- Cheque Helper/ChequeConverterView.swift
+++ Cheque Helper/ChequeConverterView.swift
@@ -4,6 +4,15 @@ import Combine
 struct ChequeConverterView: View {
     
     var body: some View {
+        VStack {
+            TextField(NSLocalizedString("Amount", comment: ""), text: $stateStore.amountString)
+            Picker(selection: $stateStore.selectedLanguage, label: Text("")) {
+                Text(NSLocalizedString("English", comment: "")).tag(0)
+                Text(NSLocalizedString("中文", comment: "")).tag(1)
+            }
+            Text(stateStore.convertedString)
+            Spacer()
+        }.padding()
     }
 }
 
