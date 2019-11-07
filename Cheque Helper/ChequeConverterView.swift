b2672d3673d51ce4197b55215398f5017fecc497
Mulang Su <sumulang@gmail.com>
2019-11-07 22:04:55 +0000

    configured subviews in ChequeConverterView

--- Cheque Helper/ChequeConverterView.swift
+++ Cheque Helper/ChequeConverterView.swift
@@ -2,15 +2,23 @@ import SwiftUI
 import Combine
 
 struct ChequeConverterView: View {
+    @ObservedObject var stateStore = ChequeConverterStateStore()
     
     var body: some View {
         VStack {
             TextField(NSLocalizedString("Amount", comment: ""), text: $stateStore.amountString)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.decimalPad)
             Picker(selection: $stateStore.selectedLanguage, label: Text("")) {
                 Text(NSLocalizedString("English", comment: "")).tag(0)
                 Text(NSLocalizedString("中文", comment: "")).tag(1)
             }
+                .pickerStyle(SegmentedPickerStyle())
+                .fixedSize()
             Text(stateStore.convertedString)
+                .font(Font.custom("Times New Roman Italic", size: 20))
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                .lineLimit(nil)
             Spacer()
         }.padding()
     }
