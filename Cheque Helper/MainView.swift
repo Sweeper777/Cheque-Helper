584b4eddb8b39d3a710b7be4cd7dc32ea8cccd6e
Mulang Su <sumulang@gmail.com>
2019-11-11 09:08:14 +0000

    added showRateMsg in main view

--- Cheque Helper/MainView.swift
+++ Cheque Helper/MainView.swift
@@ -15,6 +15,23 @@ struct MainView: View {
                 )
         }
     }
+    
+    func showRateMsg() {
+        UIApplication.shared.endEditing()
+        let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(showCloseButton: false))
+        alert.addButton(NSLocalizedString("Rate!", comment: "")) {
+            UIApplication.shared.open(
+                URL(string: "https://itunes.apple.com/us/app/pocket-cheque-helper/id1072718086?mt=8")!,
+                options: [:], completionHandler: nil)
+        }
+        alert.addButton(NSLocalizedString("Send Feedback", comment: "")) {
+            UIApplication.shared.open(
+                URL(string: "mailto:sumulang.apps@gmail.com?subject=Cheque Helper Feedback".addingPercentEncoding(withAllowedCharacters: CharacterSet.urlPathAllowed)!)!,
+                options: [:], completionHandler: nil)
+        }
+        alert.addButton(NSLocalizedString("Maybe Later", comment: ""), action: {})
+        _ = alert.showCustom(NSLocalizedString("Enjoying Cheque Helper?", comment: ""), subTitle: NSLocalizedString("You can rate this app, or send me feedback!", comment: ""), color: UIColor(hex: "5abb5a"), circleIconImage: UIImage())
+    }
 }
 
 struct MainView_Previews: PreviewProvider {
